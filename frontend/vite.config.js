a1bafc08e44b24a852681ae0222df65ece285c2f
Guilherme Faustino Da Silva <128479482+Guilherme-FDS@users.noreply.github.com>
2026-05-22 14:21:56 -0300

    Update Vite config by removing server proxy
    Removed server proxy configuration from Vite config.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,13 +6,5 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
   }
 })
