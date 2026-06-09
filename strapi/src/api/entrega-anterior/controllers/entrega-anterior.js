d098bf60e439ce528eb0cd2795d579ad6cc04d6b
Guilherme-FDS <guilhermefaustino0402@hotmail.com>
2026-06-09 19:53:14 -0300

    feat: add entrega-anterior collection type and expand home schema

---
+++ strapi/src/api/entrega-anterior/controllers/entrega-anterior.js
@@ -0,0 +1,3 @@
+'use strict';
+const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::entrega-anterior.entrega-anterior');
---
+++ strapi/src/api/entrega-anterior/services/entrega-anterior.js
@@ -0,0 +1,3 @@
+'use strict';
+const { createCoreService } = require('@strapi/strapi').factories;
+module.exports = createCoreService('api::entrega-anterior.entrega-anterior');
